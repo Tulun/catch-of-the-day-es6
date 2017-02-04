3af41c9b2007184bfacc754b7f43dfdd980d292d
Tulun <jaykiraly@gmail.com>
2017-02-04 11:36:50 -0800

    Created first component.

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,14 @@
-// let's go!
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class StorePicker extends Component {
+  
+  render() {
+    return (
+      <p>Hello</p>
+    );
+  }
+
+}
+
+render(<StorePicker />, document.querySelector('#main'));
