8128acdaa510027b42d1a78caf3368d2c51242ec
Tulun <jaykiraly@gmail.com>
2017-02-06 11:15:53 -0800

    Added new order/app/inventory/header components.

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class StorePicker extends Component {
+  
+  render() {
+    return (
+      <div>
+
+        <form className='store-selector'>
+          <h2>Please Enter a Store</h2>
+          <input type='text' required placeholder='Store Name' />
+          <button type='submit'>Visit Store ➞</button>
+        </form>
+
+      </div>
+    );
+  }
+
+}
+
+export default StorePicker;
