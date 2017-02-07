b7959848ea1ddfa31817fa750fee486c9785e783
Tulun <jaykiraly@gmail.com>
2017-02-07 14:09:09 -0800

    Connected up fish form logic to app state using props

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -5,6 +5,35 @@ import Inventory from './Inventory';
 import Order from './Order';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.addFish = this.addFish.bind(this); 
+    this.state = {
+      
+      fishes: {
+
+      },
+      order: {
+
+      }
+
+    };
+  }
+
+  addFish(fish) {
+    //update our state
+    const fishes = {...this.state.fishes};
+
+    // add in our new fish
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+
+    // set state
+    // note: You can use { fishes } instead of { fishes: fishes } if the
+    // the namespacing is the same.
+    this.setState({ fishes });
+  }
   render() {
     return (
       <div className="catch-of-the-day">
@@ -13,7 +42,7 @@ class App extends Component {
           <Header tagline='Fresh Seafood Market' />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
