0a8deb0db9262a3db27df0680427045b7165585c
Tulun <jaykiraly@gmail.com>
2017-02-07 13:10:24 -0800

    Created basic add fish form.

---
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class AddFishForm extends Component {
+
+  createFish(event) {
+    event.preventDefault();
+    console.log('Gonna make some fish!');
+  }
+
+  render() {
+    return (
+      <form className="fish-edit" onSubmit={ (e) => this.createFish(e) }>
+        <input type="text" placeholder='Fish Name'/>
+        <input type="text" placeholder='Fish Price'/>
+        <select>
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out!</option>
+        </select>
+        <textarea placeholder='Fish Desc'/>
+        <input type="text" placeholder='Fish Image'/>
+        <button type='submit'>+ Add Item</button>
+      </form>
+    );
+  }
+}
+
+export default AddFishForm;
