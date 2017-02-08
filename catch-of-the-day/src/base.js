04fe27107b0668d53d9ffbb1ed315b6a3baaaa15
Tulun <jaykiraly@gmail.com>
2017-02-08 11:42:16 -0800

    Fishes persist on firebase.

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyB9zDYv2sGRnc2V2eQKnU0x2ua2eACiJFw",
+  authDomain: "catch-of-the-day-es6-jaykir.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-es6-jaykir.firebaseio.com",
+});
+
+export default base;
