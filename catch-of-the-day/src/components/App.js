2744e1fa0d2cacde8faf34e5ab4778b8d05e4cfc
Tulun <jaykiraly@gmail.com>
2017-02-07 20:59:58 -0800

    Order state updates when you click 'add fish'

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -13,6 +13,7 @@ class App extends Component {
 
     this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
+    this.addToOrder = this.addToOrder.bind(this);
 
     this.state = {
       
@@ -46,6 +47,17 @@ class App extends Component {
     });
   }
 
+  addToOrder(key) {
+    // take a copy of our state
+    const order = {...this.state.order};
+
+    // update or add the new number of fish ordered
+    order[key] = order[key] + 1 || 1;
+
+    // update our state
+    this.setState({ order })
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -53,8 +65,11 @@ class App extends Component {
         <div className="menu">
           <Header tagline='Fresh Seafood Market' />
           <ul className="list-of-fishes">
-            { Object.keys(this.state.fishes)
-             .map(key => <Fish key={key} details={this.state.fishes[key]} /> )}
+            {Object.keys(this.state.fishes)
+             .map(key => <Fish key={key}
+              index={key}
+              details={this.state.fishes[key]}
+              addToOrder={this.addToOrder} /> )}
           </ul>
         </div>
         <Order />
